e30b9e54e2d856b2f158d5ef7f6c94ccc649eabb
林韋辰 <harry9751200@gmail.com>
2018-12-18 10:31:17 +0800

    member storyboard修正

--- IosBonusRunningBeforeYou/Main/Common/Communicator.swift
+++ IosBonusRunningBeforeYou/Main/Common/Communicator.swift
@@ -25,13 +25,13 @@ typealias DownloadDoneHandler = (_ result:Data?, _ error: Error?) -> Void
 class Communicator {
    
 
-    let SOCKET_URL = "ws://192.168.196.254:8080/Running_MySQL_Web/ChatServer/" //5g
+    let SOCKET_URL = "ws://192.168.50.170:8080/Running_MySQL_Web/ChatServer/" //5g
 
 //    let SOCKET_URL = "ws://172.20.10.9:8080/Running_MySQL_WebOld/ChatServer/" //家
     
-    let GOFRIENDS_SOCKET_URL = "ws://192.168.197.55:8080/Running_MySQL_Web/GoFriendsServer/"
+    let GOFRIENDS_SOCKET_URL = "ws://192.168.50.170:8080/Running_MySQL_Web/GoFriendsServer/"
 
-    static let BASEURL = "http://192.168.197.55:8080/Running_MySQL_Web"//ip自己要再改 手機
+    static let BASEURL = "http://192.168.50.170:8080/Running_MySQL_Web"//ip自己要再改 手機
 
 //    static let BASEURL = "http://192.168.196.127:8080/Running_MySQL_Web"//ip自己要再改 III
 
